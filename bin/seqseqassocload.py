605d8c8387ca47513b20da0c5fd2a48266661558
Sharon Giannatto <Sharon.Giannatto@jax.org>
2010-02-23 18:47:30 +0000

    seqseqassocload-4-3-3-1

---
+++ bin/seqseqassocload.py
@@ -0,0 +1,258 @@
+#!/usr/local/bin/python
+
+##########################################################################
+#
+# Purpose:
+#       Load Sequence-Sequence Associations to SEQ_Sequence_Assoc
+#
+# Usage: seqseqassocload.py
+# Env Vars:
+#	 1. OUTPUTDIR
+#	 2. MGD_DBSERVER
+#	 3. MGD_DBNAME
+#	 4. MGD_DBPASSWORDFILE
+#	 5. MGD_DBUSER
+#	 6. TABLE
+#	 7. INFILE_NAME
+#	 8. SEQ1_LOGICAL_DBKEY
+#	 9. SEQ2_LOGICAL_DBKEY
+#
+# Inputs:
+#	1. tab-delimited in following format:
+#	    1. seqId1
+#           2. qualifier
+#           3. seqId2
+#	2. Configuration 
+#
+# Outputs:
+#	 1. SEQ_Sequence_Assoc bcp file
+#	 2. log file
+# 
+# Exit Codes:
+#
+#      0:  Successful completion
+#      1:  An exception occurred
+#
+#  Assumes:  Nothing
+#
+#  Notes:  None
+#
+###########################################################################
+
+import sys
+import os
+import mgi_utils
+import loadlib
+import string
+import db
+#
+# Constants
+#
+TAB= '\t'
+CRT = '\n'
+
+# Qualifier vocabulary key
+QUAL_VOCAB_KEY = 78
+
+# MGI_User key for the load
+USER_KEY = 1499
+
+#
+# GLOBALS
+#
+
+# database connection info
+mgdServer = os.environ['MGD_DBSERVER']
+mgdDB = os.environ['MGD_DBNAME']
+passwdfile = os.environ['MGD_DBPASSWORDFILE']
+password = string.strip(open(passwdfile, 'r').readline())
+user = os.environ['MGD_DBUSER']
+
+# the table we are  loading
+table = os.environ['TABLE']
+
+# timestamp for creation/modification date
+cdate = mgi_utils.date('%m/%d/%Y')      # current date
+
+# file descriptors
+# input file 
+inFile = ''
+
+# bcp file
+outFile = ''
+
+# Lookup to verify seq1 sequence {seqId:seqKey, ...}
+seq1Lookup = {} 
+
+# Qualifier lookup {term:termKey, ...}
+qualLookup = {}
+
+# Lookup to verify seq2 sequence {seqId:seqKey, ...}
+seq2Lookup = {}
+
+# current available _Assoc_key
+nextKey= ''
+
+def init():
+    # Purpose: initialize file descriptors, create connection to
+    #  the database and load lookups
+    # Returns: nothing
+    # Assumes: nothing
+    # Effects: gives SQL commands to the database
+    # Throws: nothing
+    global inFile, outFile, nextKey
+
+    print '%s' % mgi_utils.date()
+    print 'Initializing'
+   
+    db.useOneConnection(1)
+    db.set_sqlLogin(user, password, mgdServer, mgdDB)
+ 
+    results = db.sql('''select nextKey = max(_Assoc_key) + 1 '''
+		'''from %s''' % table, 'auto')
+    nextKey = results[0]['nextKey']
+    if nextKey == None:
+	nextKey = 1001
+    #print 'nextKey: %s' % nextKey
+
+    inFilePath = os.environ['INFILE_NAME']
+    try:
+	inFile = open(inFilePath, 'r')
+    except:
+	exit('Could not open file for reading %s\n' % inFilePath)
+
+    outFilePath = '%s/%s.bcp' % (os.environ['OUTPUTDIR'],  table)
+    try:
+	outFile = open(outFilePath, 'w')
+    except:
+        exit('Could not open file for writing %s\n' % outFilePath)
+
+    loadLookups()
+
+def loadLookups():
+    # Purpose: loads lookups
+    # Returns: nothing
+    # Assumes: connection has been made to the database
+    # Effects: gives SQL commands to the database
+    # Throws: nothing
+
+    global seq1Lookup, qualLookup, seq2Lookup
+
+    seq1LdbKey = os.environ['SEQ1_LOGICAL_DBKEY']
+    seq2LdbKey = os.environ['SEQ2_LOGICAL_DBKEY']
+    #print 'seq1LdbKey %s seq2LdbKey %s' % (seq1LdbKey, seq2LdbKey)
+    #
+    # Load seq1Lookup
+    #
+    results = db.sql('''select _Object_key as _Sequence_key, accID '''
+	'''from ACC_Accession '''
+	'''where _MGIType_key = 19 '''
+	'''and preferred = 1 '''
+	'''and _LogicalDB_key = %s''' % seq1LdbKey, 'auto')
+    for r in results:
+        seqKey = r['_Sequence_key']
+        seqId = r['accID']
+	seq1Lookup[seqId] = seqKey
+    #
+    # Load qualLookup
+    #
+    results = db.sql('''select _Term_key, term '''
+		'''from VOC_Term '''
+		'''where _Vocab_key = %s''' % QUAL_VOCAB_KEY, 'auto')
+    for r in results:
+	qualKey = r['_Term_key']
+	qualifier = r['term']
+	qualLookup[qualifier] = qualKey
+    #
+    # Load seq2Lookup
+    #
+    results = db.sql('''select _Object_key as _Sequence_key, accID '''
+        '''from ACC_Accession '''
+        '''where _MGIType_key = 19 '''
+        '''and preferred = 1 '''
+        '''and _LogicalDB_key = %s''' % seq2LdbKey, 'auto')
+    for r in results:
+        seqKey = r['_Sequence_key']
+        seqId = r['accID']
+	#print 'seq2Lookup loading "%s"' % seqId
+        seq2Lookup[seqId] = seqKey
+
+def deleteByUser():
+    # Purpose: delete records created by current load
+    # Returns: nothing
+    # Assumes: a connection has been made to the database
+    # Effects: deletes records from a database
+    # Throws: nothing
+
+    print '%s' % mgi_utils.date()
+    print 'Deleting records for this user'
+    db.sql('''delete from %s '''
+	'''where _CreatedBy_key = %s''' % (table, USER_KEY), None)
+
+def run():
+    # Purpose: iterate through the input file, resolving to keys
+    #  and writing to a bcp file
+    # Returns: nothing
+    # Assumes: inFile and outFile are valid file descriptors
+    # Effects: writes to the file system
+    # Throws: nothing
+
+    global nextKey
+    print '%s' % mgi_utils.date()
+    print 'Creating bcp file'
+    for line in inFile.readlines():
+        (seqId1, qualifier, seqId2) = string.split(line, TAB)
+	seqId1 = string.strip(seqId1)
+	qualifier = string.strip(qualifier)
+	seqId2 = string.strip(seqId2)
+	#print "%s %s %s" % (seqId1, qualifier, seqId2)
+        if seq1Lookup.has_key(seqId1):
+	    seqKey1 = seq1Lookup[seqId1]
+	else:
+	    print 'SeqId1 %s is not in the database' % seqId1
+	    continue
+	if qualLookup.has_key(qualifier):
+	    qualKey = qualLookup[qualifier]
+	    #print '%s: %s' % (qualifier, qualKey)
+	else:
+	    print 'Qualifier %s is not in the database' % qualifier
+	    continue 
+ 	if seq2Lookup.has_key(seqId2):
+	    seqKey2 = seq2Lookup[seqId2]
+	    #print 'SeqId2 %s IS in the database' % seqId2
+	else:
+            print 'SeqId2 %s is not in the database' % seqId2
+	    continue
+	#print "%s %s %s" % (seqKey1, qualKey, seqKey2)
+	outFile.write('%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s' % \
+	    (nextKey, TAB, seqKey1, TAB, qualKey, TAB, seqKey2, \
+		TAB, USER_KEY, TAB, USER_KEY, TAB, cdate, TAB, cdate, CRT))
+	nextKey = nextKey + 1
+
+# Purpose: Perform cleanup steps for the script.
+# Returns: Nothing
+# Assumes: Nothing
+# Effects: Nothing
+# Throws: Nothing
+
+def finalize():
+    # Purpose: closes file descriptors and connection to the db
+    # Returns: nothing
+    # Assumes: nothing
+    # Effects: nothing
+    # Throws: nothing
+
+    global inFile, outFile
+    db.useOneConnection(0)
+    inFile.close()
+    outFile.close()
+
+#
+# Main
+#
+init()
+run()
+deleteByUser()
+finalize()
+
+print '%s' % mgi_utils.date()
---
+++ bin/seqseqassocload.sh
@@ -0,0 +1,192 @@
+#!/bin/sh
+#
+#  seqseqassocload.sh
+###########################################################################
+#
+#  Purpose:  This script controls the execution of the Sequence-
+#		Sequence Association loads            
+#
+  Usage="Usage: seqseqassocload.sh config_file"
+#
+#     e.g. "seqseqassocload.sh vegaprotein_seqseqassocload.config
+#
+#  Env Vars:
+#
+#      See the configuration file
+#
+#  Inputs:
+#
+#      - Common configuration file -
+#		/usr/local/mgi/live/mgiconfig/master.config.sh
+#      - Common load configuration file - common.config
+#      - Specific load configuration file - 
+#		e.g. vegaprotein_seqseqassocload.config
+#      - association input file 
+#
+#  Outputs:
+#
+#      - An archive file
+#      - Log files defined by the environment variables ${LOG_PROC},
+#        ${LOG_DIAG}, ${LOG_CUR} and ${LOG_VAL}
+#      - BCP files for for inserts to SEQ_Sequence_Assoc table
+#      - Records written to the database tables
+#      - Exceptions written to standard error
+#      - Configuration and initialization errors are written to a log file
+#        for the shell script
+#
+#  Exit Codes:
+#
+#      0:  Successful completion
+#      1:  Fatal error occurred
+#      2:  Non-fatal error occurred
+#
+#  Assumes:  Nothing
+#
+#  Implementation:  
+#
+#  Notes:  None
+#
+###########################################################################
+
+#
+#  Set up a log file for the shell script in case there is an error
+#  during configuration and initialization.
+#
+
+cd `dirname $0`/..
+LOG=`pwd`/seqseqassocload.log
+rm -f ${LOG}
+
+#
+#  Verify the argument(s) to the shell script.
+#
+if [ $# -ne 1 ]
+then
+    echo ${Usage} | tee -a ${LOG}
+    exit 1
+fi
+
+#
+# Globals
+#
+TABLE=SEQ_Sequence_Assoc
+COLDELIM="\t"
+LINEDELIM="\n"
+
+export TABLE COLDELIM LINEDELIM
+
+#
+#  Establish the configuration file names.
+#
+CONFIG_LOAD=`pwd`/$1
+CONFIG_LOAD_COMMON=`pwd`/common.config
+
+#
+#  Make sure the configuration files are readable
+#
+
+if [ ! -r ${CONFIG_LOAD} ]
+then
+    echo "Cannot read configuration file: ${CONFIG_LOAD}"
+    exit 1
+fi
+
+if [ ! -r ${CONFIG_LOAD_COMMON} ]
+then
+    echo "Cannot read configuration file: ${CONFIG_LOAD_COMMON}"
+    exit 1
+fi
+
+#
+# source config files - order is important
+#
+. ${CONFIG_LOAD_COMMON}
+. ${CONFIG_LOAD}
+
+#
+#  Source the DLA library functions.
+#
+if [ "${DLAJOBSTREAMFUNC}" != "" ]
+then
+    if [ -r ${DLAJOBSTREAMFUNC} ]
+    then
+        . ${DLAJOBSTREAMFUNC}
+    else
+        echo "Cannot source DLA functions script: ${DLAJOBSTREAMFUNC}"
+        exit 1
+    fi
+else
+    echo "Environment variable DLAJOBSTREAMFUNC has not been defined."
+    exit 1
+fi
+
+#
+# check that INFILE_NAME has been set and exists
+#
+if [ "${INFILE_NAME}" = "" ]
+then
+     # set STAT for endJobStream.py called from postload in shutDown
+    STAT=1
+    checkStatus ${STAT} "INFILE_NAME not defined"
+fi
+
+if [ ! -r ${INFILE_NAME} ]
+then
+    # set STAT for endJobStream.py called from postload in shutDown
+    STAT=1
+    checkStatus ${STAT} "Input file: ${INFILE_NAME} does not exist"
+fi
+
+##################################################################
+##################################################################
+#
+# main
+#
+##################################################################
+##################################################################
+
+#
+# createArchive including OUTPUTDIR, startLog, getConfigEnv, get job key
+#
+
+preload ${OUTPUTDIR}
+
+#
+# rm files and dirs from OUTPUTDIR and RPTDIR
+#
+
+cleanDir ${OUTPUTDIR} ${RPTDIR}
+
+#
+# Create the Sequence-Sequence Association Load bcp file 
+# and delete old associations
+#
+
+echo "\n`date`" >> ${LOG_DIAG} 
+echo "\n`date`" >> ${LOG_PROC}
+configName=`basename  ${CONFIG_LOAD}`
+echo "Running seqseqassocload ${configName}." | tee -a ${LOG_DIAG} ${LOG_PROC}
+
+# run the load
+${SEQSEQASSOCLOAD}/bin/seqseqassocload.py >>  ${LOG_DIAG}
+STAT=$?
+checkStatus ${STAT} "creating bcp file"
+
+# bcp in
+date | tee -a ${LOG_DIAG}
+echo "" | tee -a ${LOG_DIAG}
+${MGI_DBUTILS}/bin/bcpin.csh ${MGD_DBSERVER} ${MGD_DBNAME} ${TABLE} ${OUTPUTDIR} ${TABLE}.bcp ${COLDELIM} ${LINEDELIM} >> ${LOG_DIAG} 2>&1
+STAT=$?
+if [ ${STAT} -ne 0 ]
+then
+    echo "${MGI_DBUTILS}/bin/bcpin.csh failed" | tee -a ${LOG_DIAG}
+    exit 1
+fi
+
+#
+# run postload cleanup and email logs
+#
+shutDown
+
+exit 0
+
